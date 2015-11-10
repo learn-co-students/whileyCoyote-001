e535ad4e306f435b254097bd94834cb5d500a227
richardgmartin <richardgeorgemartin@gmail.com>
2015-11-10 16:36:10 -0500

    completed whiley coyote loop coding exercises

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -5,11 +5,25 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    /**
-     
-     * Write your loop here!
+    NSUInteger steps = 0;
     
-     */
+    NSUInteger anvil = arc4random_uniform(25)+26;
+    
+    do {
+        
+        if ((steps%10 == 0) && (steps > 0)) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        
+        if (steps == anvil) {
+            NSLog(@"SMASH!");
+        }
+        
+        NSLog(@"%li: Meep!Meep", steps);
+        
+        steps++;
+        
+    } while (steps <= anvil);
     
     // do not alter
     return YES;  //
